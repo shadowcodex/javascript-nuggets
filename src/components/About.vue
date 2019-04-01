529c96fddc1cf556bfdc938c5e75614c795fa1e6
shadowcodex <jsduncan98@gmail.com>
2019-03-31 21:03:09 -0500

    comma nazi got me

--- src/components/About.vue
+++ src/components/About.vue
@@ -3,7 +3,7 @@
     <h2>What is this?</h2>
     <p
       class="mt-5"
-    >Simply put this is a collection of tweets. Each one will be a twitter thread, that acts sort of like a blog post.</p>
+    >Simply put, this is a collection of tweets. Each one will be a twitter thread, that acts sort of like a blog post.</p>
     <p
       class="mt-5"
     >The first tweet in the thread will include an example image for the topic. It is then followed by several tweets of explanations. That is it. Simple and sweet learning.</p>
